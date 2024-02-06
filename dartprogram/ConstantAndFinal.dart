5bfb1e91177c9fcc61786ff960cc74e1a386ff74
Sachin <Sachinsammy1212@gmail.com>
2024-02-06 16:37:22 +0530

    String literal and interpolation

---
+++ dartprogram/ConstantAndFinal.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  final countryName = "India";
+  const PI = 3.14;
+
+  print("Country name is $countryName and value of PI is $PI");
+}
+
+ class MyClass{
+
+  final name = "Sachin";
+  // we can't declare constant in class without static keyword
+  //const PI = 3.14;
+  static const PI = 3.14;
+}
