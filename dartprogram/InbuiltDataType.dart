a301543fe0b11a88fe5bad5c7b68d6df8c9061b6
Sachin <Sachinsammy1212@gmail.com>
2024-02-06 14:01:07 +0530

    Inbuilt data type

---
+++ dartprogram/InbuiltDataType.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  //Different data type in dart
+  String name = 'SACHIN';
+  double percentage = 90.1;
+  bool isPass = true;
+  int age = 26;
+  var phoneNumber = 7409324972;
+
+print("Name is " + name);
+print("Percentage is "+ percentage.toString());
+print("Pass status "+ isPass.toString());
+print("Age is "+ age.toString());
+print("Phone number is "+ phoneNumber.toString());
+}
